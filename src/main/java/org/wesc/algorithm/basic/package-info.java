064348ded8f41c521a195220f82987476bcbf5e6
zhangwei <zhangw985@chinaunicom.cn>
2019-03-20 15:25:47 +0800

    create a java algorithm project

---
+++ src/main/java/org/wesc/algorithm/basic/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * @Disp:
+ * @Author: zhangwei
+ * @Date: 2019/3/20 15:21
+ */
+package org.wesc.algorithm.basic;
+
+//https://www.cnblogs.com/tonylp/archive/2013/03/20/2971272.html
---
+++ src/main/java/org/wesc/algorithm/classic/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * @Disp:
+ * @Author: zhangwei
+ * @Date: 2019/3/20 15:21
+ */
+package org.wesc.algorithm.classic;
+
+//https://www.cnblogs.com/qy5201314/archive/2012/07/21/2602228.html
